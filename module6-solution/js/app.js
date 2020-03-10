b15b21e364e15fd6afc04f748b12e92f9fe9207e
azuelsdorf16 <azuelsdorf16@gmail.com>
2020-03-09 23:25:56 -0400

    Adding module 6 first cut.

---
+++ module6-solution/js/app.js
@@ -0,0 +1,29 @@
+//Use IIFE to prevent the need for local variables to enter the global scope.
+(function () {
+    'use strict';
+
+    var dependencies = [];
+    var module = angular.module("application_name", dependencies)
+
+    module.controller("controller_name", controllerLogic);
+
+    var controllerLogic = function ($scope) {
+        $scope.checkIfTooMuch = function () {
+            if ($scope.items == null || $scope.items == "") {
+                $scope.message = "Please enter data first";
+            } else {
+                var itemsList = items.split(",").filter(function (item) {
+                    return item.trim() != "";
+                });
+
+                if (itemsList.length > 3) {
+                    $scope.message = "Too much!";
+                } else {
+                    $scope.message = "Enjoy!";
+                }
+            }
+        }
+    }
+
+    controllerLogic.$inject = ["$scope"];
+})();
