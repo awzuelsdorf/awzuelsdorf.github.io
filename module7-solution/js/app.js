5268f4929183a1be75d1e74a23abe5095567f7f2
azuelsdorf16 <azuelsdorf16@gmail.com>
2020-03-15 15:06:19 -0400

    First cut of assignment.

---
+++ module7-solution/js/app.js
@@ -0,0 +1,66 @@
+//Use IIFE to prevent the need for local variables to enter the global scope.
+(function () {
+    'use strict';
+
+    var dependencies = [];
+    var module = angular.module("ShoppingListCheckOff", dependencies);
+
+    var toBuyControllerLogic = function ($scope) {
+        var itemBuyer = this;
+        
+        itemBuyer.toBuy = ShoppingListCheckOffService.getUnboughtItems();
+
+        itemBuyer.buyItem = function (index) {
+            ShoppingListCheckOffService.buyItem(index);
+	}
+    };
+
+    toBuyControllerLogic.$inject = ["ShoppingListCheckOffService"];
+
+    module.controller("ToBuyController", toBuyControllerLogic);
+
+    var alreadyBoughtControllerLogic = function (ShoppingListCheckOffService) {
+        var itemBought = this;
+
+        itemBought.bought = ShoppingListCheckOffService.getBoughtItems();
+    };
+
+    alreadyBoughtControllerLogic.$inject = ["ShoppingListCheckOffService"];
+
+    module.controller("AlreadyBoughtController", alreadyBoughtControllerLogic);
+
+    function ShoppingListCheckOffService() {
+        var service = this;
+
+        var toBuy = [
+            {
+                "name": "Cookies",
+                "quantity": 10,
+                "unit": "boxes",
+                "pricePerItem": 4.50
+            },
+            {
+                "name": "Pepto Bismol",
+                "quantity": 3,
+                "unit": "bottles",
+                "pricePerItem": 10.90
+            },
+	];
+
+        var bought = [];
+
+        service.buyItem = function (itemIndex) {
+            service.bought.push(service.toBuy.pop(itemIndex));
+	}
+
+        service.getUnboughtItems = function () {
+            return service.toBuy;
+	}
+
+        service.getBoughtItems = function () {
+            return service.bought;
+	}
+    }
+
+    module.service("ShoppingListCheckOffService", ShoppingListCheckOffService);
+})();
