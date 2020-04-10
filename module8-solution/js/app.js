db01a4c19dfafc0263b504af9fdd1527afa4bac0
azuelsdorf16 <azuelsdorf16@gmail.com>
2020-04-10 00:48:46 -0400

    Implementing more of the code.

--- module8-solution/js/app.js
+++ module8-solution/js/app.js
@@ -2,27 +2,60 @@
     'use strict';
 
     var dependencies = [];
-    var module = angular.module("NarrowItDownApp", dependencies)
-.controller('NarrowItDownController', NarrowItDownController).service('MenuSearchService', MenuSearchService);
+    angular.module("NarrowItDownApp", dependencies)
+.controller('NarrowItDownController', NarrowItDownController)
+.service('MenuSearchService', MenuSearchService)
+.directive('foundItems', FoundItemsDirective);
+
+    function FoundItemsDirective() {
+        var ddo = {
+            templateUrl: 'foundItems.html',
+            scope: {
+                found: '<',
+                onRemove: '&'
+            },
+            controller: NarrowItDownController,
+            controllerAs: 'narrowCtrl',
+            bindToController: true
+        };
+
+        return ddo;
+    }
+
 
     NarrowItDownController.$inject = ['MenuSearchService'];
     function NarrowItDownController(MenuSearchService) {
         var searcher = this;
 
+	searcher.found = [];
+
         search.getMatchedMenuItems = function (searchTerm) {
             return $http("https://davids-restaurant.herokuapp.com/menu_items.json").then(function (result) {
-                var foundItems = [];
+                searcher.found = [];
+
                 var allMenuItems = result["menu_items"];
 
                 for (var i = 0; i < allMenuItems.length; ++i) {
                     if (allMenuItems[i].description.indexOf(searchTerm) !== -1) {
-                        foundItems.add(allMenuItems[i]);
+                        searcher.found.push(allMenuItems[i]);
 		    }
                 }
 
-                return foundItems;
+                return searcher.found;
 	    });
         }
+
+        service.removeItem = function (itemIndex) {
+            searcher.found.splice(itemIndex, 1);
+        };
+    }
+
+    function MenuSearchService() {
+        var factory = function () {
+            return new ShoppingListService();
+        };
+
+        return factory;
     }
 }
 
