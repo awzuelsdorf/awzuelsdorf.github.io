4636036a15f4acd43e849245026ba34fa02dcd19
azuelsdorf16 <azuelsdorf16@gmail.com>
2020-04-11 16:57:33 -0400

    Implemented http retrieval, need to implement message again.

--- module8-solution/app.js
+++ module8-solution/app.js
@@ -31,12 +31,10 @@ function NarrowItDownDirectiveController() {
   };
 }
 
-
 NarrowItDownController.$inject = ['ShoppingListFactory'];
 function NarrowItDownController(ShoppingListFactory) {
   var viewList = this;
 
-  // Use factory to create new shopping list service
   var shoppingList = ShoppingListFactory();
 
   viewList.items = shoppingList.getItems();
@@ -55,6 +53,7 @@ function NarrowItDownController(ShoppingListFactory) {
       viewList.items.push(newItems[i]);
     }
 
+    console.log("hi");
     console.log(viewList.items);
     viewList.updateTitle();
   };
@@ -74,40 +73,41 @@ function NarrowItDownController(ShoppingListFactory) {
   };
 }
 
-
-function ShoppingListService() {
+function ShoppingListService($http) {
   var service = this;
 
   // List of shopping items
   var items = [];
 
+  var warning
+
   service.getMatchedMenuItems = function (searchTerm) {
-    items.splice(0, items.length);
+    return $http({method: "GET", url: "https://davids-restaurant.herokuapp.com/menu_items.json"}).then(function (result) {
+      var foundItems = [];
+      items.splice(0, items.length);
 
-    if (searchTerm == null || searchTerm.trim().length == 0) {
+      if (searchTerm == null || searchTerm.trim().length == 0) {
         return [];
-    }
-
-    //TODO: Remove this w/ the response from the endpoint
-    var menuResponse = {"menu_items":[{"id":877,"short_name":"A1","name":"Won Ton Soup with Chicken","description":"chicken broth with white meat chicken pieces","price_small":2.55,"price_large":5.0,"small_portion_name":"pint","large_portion_name":"quart"},{"id":878,"short_name":"A2","name":"Egg Drop Soup","description":"chicken broth with egg drop","price_small":2.25,"price_large":4.5,"small_portion_name":"pint","large_portion_name":"quart"},{"id":879,"short_name":"A3","name":"Chicken Corn Soup","description":"clear chicken broth with creamy corn and egg drop","price_small":2.75,"price_large":5.5,"small_portion_name":"pint","large_portion_name":"quart"},{"id":880,"short_name":"A4","name":"Hot and Sour Soup","description":"tofu, chicken, mushroom, bamboo shoot, and egg","price_small":2.55,"price_large":5.0,"small_portion_name":"pint","large_portion_name":"quart"}]};
-
-    console.log("hi");
-
-    var allItems = menuResponse["menu_items"];
-
-    var foundItems = [];
-
-    for (var i = 0; i < allItems.length; ++i) {
-      if (allItems[i]["description"].indexOf(searchTerm) !== -1) {
-        console.log(allItems[i]["description"].indexOf(searchTerm));
-        console.log(allItems[i]["description"]);
-        console.log(searchTerm);
-        items.push({name: allItems[i]["description"]});
-        foundItems.push({name: allItems[i]["description"]});
       }
-    }
 
-    return foundItems;
+      var allItems = result["data"]["menu_items"];
+
+      var foundItems = [];
+
+      for (var i = 0; i < allItems.length; ++i) {
+        if (allItems[i]["description"].indexOf(searchTerm) !== -1) {
+          console.log(allItems[i]["description"].indexOf(searchTerm));
+          console.log(allItems[i]["description"]);
+          console.log(searchTerm);
+          items.push({name: allItems[i]["description"]});
+          foundItems.push({name: allItems[i]["description"]});
+        }
+      }
+
+      return foundItems;
+    });
+
+    //var menuResponse = {"menu_items":[{"id":877,"short_name":"A1","name":"Won Ton Soup with Chicken","description":"chicken broth with white meat chicken pieces","price_small":2.55,"price_large":5.0,"small_portion_name":"pint","large_portion_name":"quart"},{"id":878,"short_name":"A2","name":"Egg Drop Soup","description":"chicken broth with egg drop","price_small":2.25,"price_large":4.5,"small_portion_name":"pint","large_portion_name":"quart"},{"id":879,"short_name":"A3","name":"Chicken Corn Soup","description":"clear chicken broth with creamy corn and egg drop","price_small":2.75,"price_large":5.5,"small_portion_name":"pint","large_portion_name":"quart"},{"id":880,"short_name":"A4","name":"Hot and Sour Soup","description":"tofu, chicken, mushroom, bamboo shoot, and egg","price_small":2.55,"price_large":5.0,"small_portion_name":"pint","large_portion_name":"quart"}]};
   };
 
   service.removeItem = function (itemIndex) {
@@ -119,10 +119,10 @@ function ShoppingListService() {
   };
 }
 
-
-function ShoppingListFactory() {
+ShoppingListFactory.$inject = ["$http"];
+function ShoppingListFactory($http) {
   var factory = function () {
-    return new ShoppingListService();
+    return new ShoppingListService($http);
   };
 
   return factory;
